c5b6c2e40e9dfcc4fdd5e747cf7104072689cb88
John Cunningham <johncunningham1214@gmail.com>
2026-07-16 21:45:49 -0400

    Fix Vercel Turbopack error

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
-import withPWAInit from "@ducanh2912/next-pwa";
 import type { NextConfig } from "next";
 
-const withPWA = withPWAInit({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  disable: process.env.NODE_ENV === "development", // Disables PWA in dev mode
-});
-
 const nextConfig: NextConfig = {
-  // Any existing config you had goes here
+  turbopack: {},
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
